0fafd1d3406abff0f09ee9e4a677c675c961a32e
Jani <n4512@student.jamk.fi>
2024-08-12 21:48:36 +0300

    adds something i dont even remember

--- src/app/diet-diary/diet-diary.component.ts
+++ src/app/diet-diary/diet-diary.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SearchFoodsService } from '../services/search-foods.service';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-diet-diary',
@@ -12,12 +14,14 @@ import { SearchFoodsService } from '../services/search-foods.service';
 })
 export class DietDiaryComponent implements OnInit {
   dietForm: FormGroup;
+  searchQuery: string = '';
   searchResults: any[] = [];
 
   constructor(private fb: FormBuilder, private searchFoodsService: SearchFoodsService) {
     this.dietForm = this.fb.group({
       meals: this.fb.array([])
     });
+    console.log('DietDiaryComponent initialized');
   }
 
   ngOnInit(): void {
@@ -32,33 +36,68 @@ export class DietDiaryComponent implements OnInit {
     const mealForm = this.fb.group({
       name: ['', Validators.required],
       food_search: [''],
-      selected_food: this.fb.array([])
+      selected_food: this.fb.array([]),
+      searchResults: [[]]
     });
     this.meals.push(mealForm);
+    console.log('Meal added:', mealForm);
   }
   onFoodSearch(event: any, index: number): void {
     const query = event.target.value;
     if (query.length > 2) {
-      this.searchFoodsService.searchFoods(query).subscribe(results => {
-        this.searchResults = results;
+      this.searchFoodsService.searchFoods(query).pipe(
+        catchError(error => {
+          console.error('Search error:', error);
+          return of([]);
+        })
+      ).subscribe(results => {
+        const meals = this.dietForm.get('meals') as FormArray;
+        const meal = meals.at(index) as FormGroup;
+        meal.patchValue({ searchResults: results });
+        //this.searchResults = results;
+        console.log(`Search results for meal ${index}:`, results);
       });
     }
   }
 
   removeMeal(index: number) {
     this.meals.removeAt(index);
+    console.log('Meal removed at index:', index);
   }
   selectFood(food: any, index: number): void {
     const selectedFoods = this.meals.at(index).get('selected_food') as FormArray;
-    selectedFoods.push(this.fb.control(food));
-    this.searchResults = [];
+
+    console.log('Selecting food:', food, 'for meal index:', index);
+
+    
+    this.searchFoodsService.fetchFoodWithId(food.id).subscribe(fetchedFood => {
+      const foodWithNutrients = {
+        ...food,
+        nutrients: {
+          energy: fetchedFood.energy || 0,
+          protein: fetchedFood.protein || 0,
+          fat: fetchedFood.fat || 0,
+          carbohydrate: fetchedFood.carbohydrate || 0,
+          fiber: fetchedFood.fiber || 0,
+          sugar: fetchedFood.sugar || 0,
+        }
+      };
+      console.log('fetchFoodWithId:', fetchedFood);
+      selectedFoods.push(this.fb.control(foodWithNutrients));
+
+      // Reset search input and results
+      const meal = this.meals.at(index) as FormGroup;
+      meal.patchValue({ food_search: '', searchResults: [] });
+      console.log('Food selected:', foodWithNutrients, 'for meal index:', index);
+    });
   }
   removeSelectedFood(index: number, foodIndex: number) {
     const selectedFoods = this.meals.at(index).get('selected_food') as FormArray;
     selectedFoods.removeAt(foodIndex);
+    console.log('Selected food removed at index:', foodIndex, 'for meal index:', index);
   }
 
   onSubmit() {
-    console.log(this.dietForm.value);
+    console.log('Form submitted:', this.dietForm.value);
   }
 }
